f6a32a0e9a56357a3b0d74d31cea94a6a9032f4a
Stuart Claghorn <sclaghorn@gmail.com>
2019-05-08 20:58:40 -0400

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Stuart Claghorn on 5/8/19.
+//  Copyright © 2019 sclaghorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
