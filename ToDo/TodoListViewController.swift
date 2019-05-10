7916412ecd50ed79595517492e85f720dc4a9431
Stuart Claghorn <sclaghorn@gmail.com>
2019-05-09 21:33:42 -0400

    Create add button and method with alert

--- ToDo/TodoListViewController.swift
+++ ToDo/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Call Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Call Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +42,32 @@ class TodoListViewController: UITableViewController {
     
         tableView.deselectRow(at: indexPath, animated: true)
     
+    }
+    
+    // MARK: Add New Items Methods
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // What will happen when the user hits add item button
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
     }
 }
 
